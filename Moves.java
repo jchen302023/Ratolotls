74fc132f60024942e132b3272fadb70d920ebcf7
jchen302023 <jchen30@stuy.edu>
2022-05-24 13:42:55 -0400

    bwee

--- Moves.java
+++ Moves.java
@@ -4,15 +4,21 @@ public class Moves {
 
 private String moveName;
 private int moveDamage;
+private PomType moveType;
 
-public Moves(String mName, int mDamage){
+public Moves(String mName, int mDamage, PomType mType){
   moveName = mName;
   moveDamage = mDamage;
+  moveType = myType; 
 }
 // ACCESSORS
 
 public int getMoveDamage() {
-  return moveDamage; 
+  return moveDamage;
+}
+
+public PomType getMoveType() {
+  return moveType;
 }
 
 }
