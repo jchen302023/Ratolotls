9e0caf37e564f9d57a16d15f49c326c204081cba
gloriaslee <glee20@stuy.edu>
2022-06-02 14:09:51 -0400

    idek plz

---
+++ v4/Moves.java
@@ -0,0 +1,48 @@
+
+public class Moves {
+
+
+protected String moveName;
+protected int moveDamage;
+protected PomType moveType;
+
+//String mName, int mDamage, PomType mType
+public Moves(){
+  // moveName = mName;
+  // moveDamage = mDamage;
+  // moveType = mType; 
+}
+// ACCESSORS
+
+public int getMoveDamage() {
+  return moveDamage;
+}
+
+public PomType getMoveType() {
+  return moveType;
+}
+
+public String getMoveName() {
+  return moveName; 
+}
+
+
+
+}
+
+
+
+class WaterGun extends Moves {
+  
+  // private String moveName;
+  // private int moveDamage;
+  // private PomType moveType;
+  
+  public WaterGun() {
+    PomType WATER = new Water(); 
+     
+    moveName = "Watergun";
+    moveDamage = 25;
+    moveType = WATER; 
+  }
+} // watergun
