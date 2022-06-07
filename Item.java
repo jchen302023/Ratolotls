3a3a70119491e929dea811d2945809f02dbdfe11
unknown <jchen30@stuy.edu>
2022-06-07 14:01:06 -0400

    i will heal you

--- Item.java
+++ Item.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 public class Item {
 
@@ -17,28 +17,27 @@ public class Item {
 
 	//return -1 if you can't afford the item or decide not to buy it
 	//return your new balance otherwise
-	public int buy( Player customer ) {
-
-		Scanner in = new Scanner( System.in );
-		String confirm;
-		if ( customer.getBalance() < price ) 
-			return -1;
-
-		//double check the player actually wants to buy the item
-		System.out.println("Are you sure you want to buy " + name + " ?\nYes\t\t\tNo");
-		try {
-			confirm = in.nextLine().toLowerCase().trim();
-		} catch ( Exception e ) {
-			System.out.println("Faulty input, cancelling buy...");
-		}
-
-		if ( confirm.equals("yes") ) {
-			customer.addToBag(this);
-			customer.changeBal( -1 * price );
-			return customer.getBalance();
-		}
-
-		return -1;
-	}
+	// public int buy( Player customer ) {
+	//
+	// 	Scanner in = new Scanner( System.in );
+	// 	String confirm;
+	// 	if ( customer.getBalance() < price )
+	// 		return -1;
+	//
+	// 	//double check the player actually wants to buy the item
+	// 	System.out.println("Are you sure you want to buy " + name + " ?\nYes\t\t\tNo");
+	// 	try {
+	// 		confirm = in.nextLine().toLowerCase().trim();
+	// 	} catch ( Exception e ) {
+	// 		System.out.println("Faulty input, cancelling buy...");
+	// 	}
+	//
+	// 	if ( confirm.equals("yes") ) {
+	// 		customer.addToBag(this);
+	// 		customer.changeBal( -1 * price );
+	// 		return customer.getBalance();
+	// 	}
+	//
+	// 	return -1;
+	// }
 }
-
